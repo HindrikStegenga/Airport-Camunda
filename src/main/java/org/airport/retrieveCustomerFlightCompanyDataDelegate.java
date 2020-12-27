c6f1a1659ea6b44d6c2931eee5d54bab07c53620
Siddhart Mitra <siddharth.snu@gmail.com>
2020-12-27 13:40:45 +0100

    added one more class

--- src/main/java/org/airport/retrieveCustomerFlightCompanyDataDelegate.java
+++ src/main/java/org/airport/retrieveCustomerFlightCompanyDataDelegate.java
@@ -4,7 +4,7 @@ import org.camunda.bpm.engine.delegate.JavaDelegate;
 public class retrieveCustomerFlightCompanyDataDelegate implements JavaDelegate {
     //Random rando = new Random();
     public void execute(DelegateExecution execution) throws Exception {
-        String enoughFunds=(String) execution.getVariable("funds");
+        String enoughFunds=(String) execution.getVariable("enoughFunds");
         execution.setVariable("yes", enoughFunds);
     }
 }
